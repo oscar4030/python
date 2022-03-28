f2fec83db09524ce10baa12c65da599f84e4db07
oscar4030 <102346886+oscar4030@users.noreply.github.com>
2022-03-28 22:06:26 +0900

    Add files via upload

---
+++ chap2-04.py
@@ -0,0 +1,40 @@
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+
+side = 300
+angle = 90
+
+t.forward(side)
+t.left(angle)
+t.forward(side)
+t.left(angle)
+t.forward(side)
+t.left(angle)
+t.forward(side)
+
+t.forward(side)
+t.left(angle)
+t.forward(side)
+t.left(angle)
+t.forward(side)
+t.left(angle)
+t.forward(side)
+
+t.forward(side)
+t.left(angle)
+t.forward(side)
+t.left(angle)
+t.forward(side)
+t.left(angle)
+t.forward(side)
+
+t.forward(side)
+t.left(angle)
+t.forward(side)
+t.left(angle)
+t.forward(side)
+t.left(angle)
+t.forward(side)
+
+
